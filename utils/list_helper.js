a22362c4d96602b6f35294f30a5ba8a980bd227c
SkanderManai <skander.manai23@gmail.com>
2024-02-06 12:14:29 +0100

    Exercise 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,7 +6,31 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return {};
+  }
+  let maxLikes = 0;
+  let maxIndex = 0;
+
+  for (let i = 0; i < blogs.length; i++) {
+    if (blogs[i].likes > maxLikes) {
+      maxLikes = blogs[i].likes;
+      maxIndex = i;
+    }
+  }
+
+  const result = {
+    title: blogs[maxIndex].title,
+    author: blogs[maxIndex].author,
+    likes: blogs[maxIndex].likes,
+  };
+
+  return result;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
