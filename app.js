805dceabca8dace800635b7c753d32c12b0b5d02
SkanderManai <skander.manai23@gmail.com>
2024-02-21 15:11:10 +0100

    test

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
 const blogRouter = require("./controllers/blogList");
+const usersRouter = require("./controllers/users");
 const logger = require("./utils/logger");
 const errorHandler = require("./utils/middleware");
 
@@ -20,6 +21,7 @@ mongoose
 app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogRouter);
+app.use("/api/users", usersRouter);
 app.use(errorHandler);
 
 module.exports = app;
