91c707d69def32c825ae43c7e944e58802726dd1
SkanderManai <skander.manai23@gmail.com>
2024-02-29 14:48:52 +0100

    Exercice 17

--- controllers/blogList.js
+++ controllers/blogList.js
@@ -1,22 +1,31 @@
 const blogRouter = require("express").Router();
 const Blog = require("../models/Blog");
+const User = require("../models/User");
 
 blogRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
   response.json(blogs);
 });
 
 blogRouter.post("/", async (request, response) => {
   const body = request.body;
   // console.log(body);
+
+  const users = await User.find({});
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: Number(body.likes) || 0,
+    user: users[1]._id,
   });
 
   const savedBlog = await blog.save();
+
+  users[1].blogs = users[1].blogs.concat(savedBlog._id);
+  await users[1].save();
+
   response.status(201).json(savedBlog);
 });
 
--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,11 @@ const User = require("../models/User");
 const passwordValidator = require("../utils/passwordValidator");
 
 usersRouter.get("/", async (req, res) => {
-  const users = await User.find({});
+  const users = await User.find({}).populate("blogs", {
+    title: 1,
+    url: 1,
+    author: 1,
+  });
   res.json(users);
 });
 
