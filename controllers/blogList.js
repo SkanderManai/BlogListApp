82c467332d12af81423d57476815b2e20dcdac1a
SkanderManai <skander.manai23@gmail.com>
2024-02-29 20:46:27 +0100

    Exercise 4.21

--- controllers/blogList.js
+++ controllers/blogList.js
@@ -3,14 +3,14 @@ const jwt = require("jsonwebtoken");
 const Blog = require("../models/Blog");
 const User = require("../models/User");
 
-const getTokenFrom = (req) => {
-  const authorization = req.get("authorization");
-  if (authorization && authorization.startsWith("Bearer ")) {
-    return authorization.replace("Bearer ", "");
-  }
+// const getTokenFrom = (req) => {
+//   const authorization = req.get("authorization");
+//   if (authorization && authorization.startsWith("Bearer ")) {
+//     return authorization.replace("Bearer ", "");
+//   }
 
-  return null;
-};
+//   return null;
+// };
 
 blogRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
@@ -21,7 +21,7 @@ blogRouter.post("/", async (request, response) => {
   const body = request.body;
   // console.log(body);
 
-  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  const decodedToken = jwt.verify(request.token, process.env.SECRET);
   if (!decodedToken.id) {
     return response.status(401).json({ error: "token invalid" });
   }
@@ -41,6 +41,21 @@ blogRouter.post("/", async (request, response) => {
   await user.save();
 
   response.status(201).json(savedBlog);
+});
+
+blogRouter.delete("/:id", async (req, res) => {
+  const blogToDelete = await Blog.findById(req.params.id);
+
+  const decodedToken = jwt.verify(req.token, process.env.SECRET);
+
+  if (!(blogToDelete.user.toString() === decodedToken.id.toString())) {
+    return res
+      .status(401)
+      .json({ error: "blog can only be deleted by creator" });
+  }
+
+  await Blog.findByIdAndDelete(req.params.id);
+  res.status(204).end();
 });
 
 module.exports = blogRouter;
