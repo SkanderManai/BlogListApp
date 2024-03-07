ab2d485090967247a38c3b7f83e16f5ba96b6b82
SkanderManai <skander.manai23@gmail.com>
2024-03-07 23:54:40 +0100

    Exercise 5.8

--- controllers/blogList.js
+++ controllers/blogList.js
@@ -13,49 +13,61 @@ const User = require("../models/User");
 // };
 
 blogRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
-  response.json(blogs);
+    const blogs = await Blog.find({}).populate("user", {
+        username: 1,
+        name: 1,
+    });
+    response.json(blogs);
 });
 
 blogRouter.post("/", async (request, response) => {
-  const body = request.body;
-  // console.log(body);
+    const body = request.body;
+    // console.log(body);
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: "token invalid" });
-  }
-  const user = await User.findById(decodedToken.id);
+    const decodedToken = jwt.verify(request.token, process.env.SECRET);
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: "token invalid" });
+    }
+    const user = await User.findById(decodedToken.id);
 
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: Number(body.likes) || 0,
-    user: user._id,
-  });
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: Number(body.likes) || 0,
+        user: user._id,
+    });
 
-  const savedBlog = await blog.save();
+    const savedBlog = await blog.save();
 
-  user.blogs = user.blogs.concat(savedBlog._id);
-  await user.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
 
-  response.status(201).json(savedBlog);
+    response.status(201).json(savedBlog);
 });
 
 blogRouter.delete("/:id", async (req, res) => {
-  const blogToDelete = await Blog.findById(req.params.id);
+    const blogToDelete = await Blog.findById(req.params.id);
 
-  const decodedToken = jwt.verify(req.token, process.env.SECRET);
+    const decodedToken = jwt.verify(req.token, process.env.SECRET);
 
-  if (!(blogToDelete.user.toString() === decodedToken.id.toString())) {
-    return res
-      .status(401)
-      .json({ error: "blog can only be deleted by creator" });
-  }
+    if (!(blogToDelete.user.toString() === decodedToken.id.toString())) {
+        return res
+            .status(401)
+            .json({ error: "blog can only be deleted by creator" });
+    }
 
-  await Blog.findByIdAndDelete(req.params.id);
-  res.status(204).end();
+    await Blog.findByIdAndDelete(req.params.id);
+    res.status(204).end();
+});
+
+blogRouter.put("/:id", async (req, res) => {
+    const updatedBlog = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        context: "query",
+    });
+    res.json(updatedBlog);
 });
 
 module.exports = blogRouter;
