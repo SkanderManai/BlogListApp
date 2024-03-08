86402fd8cecf4b7e2590964fa721e5473c994c38
SkanderManai <skander.manai23@gmail.com>
2024-03-08 14:14:57 +0100

    small adjust

--- controllers/login.js
+++ controllers/login.js
@@ -4,26 +4,28 @@ const loginRouter = require("express").Router();
 const User = require("../models/User");
 
 loginRouter.post("/", async (req, res) => {
-  const { username, password } = req.body;
+    const { username, password } = req.body;
 
-  const user = await User.findOne({ username });
-  const passwordCorrect =
-    user === null ? false : await bcrypt.compare(password, user.passwordHash);
+    const user = await User.findOne({ username });
+    const passwordCorrect =
+        user === null
+            ? false
+            : await bcrypt.compare(password, user.passwordHash);
 
-  if (!(user && passwordCorrect)) {
-    return res.status(401).json({
-      error: "invalid username or password",
-    });
-  }
+    if (!(user && passwordCorrect)) {
+        return res.status(401).json({
+            error: "invalid username or password",
+        });
+    }
 
-  const userForToken = {
-    username: user.username,
-    id: user._id,
-  };
+    const userForToken = {
+        username: user.username,
+        id: user._id,
+    };
 
-  const token = jwt.sign(userForToken, process.env.SECRET);
+    const token = jwt.sign(userForToken, process.env.SECRET);
 
-  res.status(200).send({ token, username: user.username, name: user.name });
+    res.status(200).send({ token, username: user.username, name: user.name });
 });
 
 module.exports = loginRouter;
